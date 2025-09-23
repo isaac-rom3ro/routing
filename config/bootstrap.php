44493540412c9b5b87ae6a95e2b822d9e7d6757e
isaac-rom3ro <is4ac.romero@outlook.com>
2025-09-23 15:41:46 -0300

    fix/settings

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 
+define('ROOT_PATH', realpath(__DIR__ . '/..'));
+
 require_once ROOT_PATH . '/vendor/autoload.php';
--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <?php
 
-define('ROOT_PATH', realpath(__DIR__));
-
 require_once __DIR__ . '/config/bootstrap.php';
 
 use App\Web\Routes;
 
 $routes = new Routes();
-$routes->addNewRoute(
-    endpoint: '/', 
-    method: 'GET', 
-    callback: function(?array $request = null) {
-        if ($request !== null) {
-            echo $request["message"];
-            exit;
-        }
 
-        echo "Loads without Query Parameters";
-    });
-// $routes->addNewRoute('/your-end-point', 'METHOD' , function() {echo 'Do somehting here!';});
+$routes->addNewRoute(
+    endpoint: '/welcome',
+    method: 'GET',
+    callback: function() {
+        echo "WELCOME";
+    }
+);
 
 $routes->run();
