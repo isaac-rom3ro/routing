78578eefc4be12ac560a33bb41fe52aabaea0334
isaac-rom3ro <is4ac.romero@outlook.com>
2025-10-09 21:58:10 -0300

    feat/Log

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 require_once __DIR__ . '/config/bootstrap.php';
 
-use App\Controllers\GenericController;
+use App\Web\Log;
 use App\DateTime\Obsid;
+use App\Controllers\GenericController;
 
 $genericController = new GenericController();
 
@@ -18,6 +19,15 @@ $router->addNewRoute(
     method: GET,
     callback: function () {
         var_dump(Obsid::now()->format('Y-m-d'));
+    }
+);
+
+$router->addNewRoute(
+    endpoint: '/log',
+    method: GET,
+    callback: function () {
+        // Log::create('erros');
+        Log::add('erros','Attempt failed to connect to database');
     }
 );
 
