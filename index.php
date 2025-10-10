7aa4388c95b252b5c8d9b32d545b302897d236ba
isaac-rom3ro <is4ac.romero@outlook.com>
2025-10-10 19:57:03 -0300

    chore: Pancake - a simple http service

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/config/bootstrap.php';
 use App\Web\Log;
 use App\DateTime\Obsid;
 use App\Controllers\GenericController;
+use App\Http\Pancake;
 
 $genericController = new GenericController();
 
@@ -18,7 +19,7 @@ $router->addNewRoute(
     endpoint: '/date',
     method: GET,
     callback: function () {
-        var_dump(Obsid::now()->format('Y-m-d'));
+        echo "Hello World";
     }
 );
 
@@ -28,6 +29,22 @@ $router->addNewRoute(
     callback: function () {
         // Log::create('erros');
         Log::add('erros','Attempt failed to connect to database');
+    }
+);
+
+$router->addNewRoute(
+    endpoint:'/pancake',
+    method: GET,
+    callback: function () {
+        Pancake::get('asd');
+    }
+);
+
+$router->addNewRoute(
+    endpoint: '/user',
+    method:GET,
+    callback: fn($parameters) => function ($parameters) {
+        
     }
 );
 
