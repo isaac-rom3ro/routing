53ced7146616a2ac70345520ef89c89f46f40078
isaac-rom3ro <is4ac.romero@outlook.com>
2025-10-23 18:51:44 -0300

    feat/favicon

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Routing System</title>
+    <link rel="shortcut icon" href="https://static.thenounproject.com/png/61289-200.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap');
